9fc486162ea4c85911583f949a6bf4b7b13afed4
wickerdj <wickerdj@gmail.com>
2025-11-16 10:55:51 -0600

    added log

--- device1/src/main.zig
+++ device1/src/main.zig
@@ -12,6 +12,17 @@ const uart = rp2xxx.uart.instance.num(1);
 const uart_tx_pin = gpio.num(4);
 const uart_rx_pin = gpio.num(5);
 
+pub fn panic(message: []const u8, _: ?*std.builtin.StackTrace, _: ?usize) noreturn {
+    std.log.err("panic: {s}", .{message});
+    @breakpoint();
+    while (true) {}
+}
+
+pub const microzig_options = microzig.Options{
+    .log_level = .debug,
+    .logFn = rp2xxx.uart.log,
+};
+
 pub fn main() !void {
     inline for (&.{ uart_tx_pin, uart_rx_pin }) |pin| {
         pin.set_function(.uart);
@@ -20,9 +31,13 @@ pub fn main() !void {
     led.set_function(.sio);
     led.set_direction(.out);
 
+    uart_tx_pin.set_function(.uart);
+
     uart.apply(.{
         .clock_config = rp2xxx.clock_config,
     });
+
+    rp2xxx.uart.init_logger(uart);
 
     while (true) {
         const msgOn = "ON\r\n";
@@ -33,6 +48,7 @@ pub fn main() !void {
             uart.clear_errors();
         };
         led.put(1);
+        std.log.info("msg: {s}", .{msgOn});
         time.sleep_ms(3000);
 
         const msgOff = "OFF\n\r";
@@ -40,6 +56,7 @@ pub fn main() !void {
             uart.clear_errors();
         };
         led.put(0);
+        std.log.info("msg: {s}", .{msgOff});
         time.sleep_ms(1000);
     }
 }
